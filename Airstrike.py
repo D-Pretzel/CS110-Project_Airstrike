3f8bcc0431770f6f4dda5c906d4e8e9791355506
D-Pretzel <petzoldavid02@gamil.com>
2021-11-29 16:48:16 -0700

    Updated scan drone pattern -- points need to be fixed

--- Airstrike.py
+++ Airstrike.py
@@ -1,15 +1,15 @@
 # Project Part 3 - Airstrike
-# Author(s): C4C Petzold & C4C Cho
+# Authors: C4C Petzold & C4C Cho
 # Course: CS110, Fall 2021
-# Final Turn-in Options: 
+# Final Turn-in Options: 1, 5
 # DOCUMENTATION: C/4C Anderson assisted in logic surrounding drone_bomber bombing behavior
 
-## ======================== ##
-#   Gate Check Requirements   #
-    #* 1. Identify the location of at least one (of severl) enemy bases by scanning with the recon drone
+'''
+#! Gate Check Requirements:
+    #* 1. Identify the location of at least one (of several) enemy bases by scanning with the recon drone
     #* 2. Use the drone bomber to attack the base identified by the recon drone (you can leave "ignore_drone_damage" on)
     #* 3. Map used must be "MAP_SEED = 'three'"
-## ======================== ##
+'''
 
 import random
 import drone_api
@@ -61,6 +61,18 @@ def get_hit_coords(targets):
     
     return(target_list)
 
+# Returns NOTHING -> simply puts data where it needs to go. Accepts 2D list as parameter
+def is_empty_scan(scan):
+    global all_targets
+    global targets_to_hit
+    
+    if len(scan) != 0:
+        all_targets += scan  # concatonates all data to all_targets (replacement for flattening) --- "all_targets" is 2D
+
+    for row in all_targets:
+        if (row[0] == 'base'):     #! Change this line to change what the bomber drone bombs
+            targets_to_hit.append(row)
+
 
 def drone_recon():
     global all_targets
@@ -75,47 +87,48 @@ def drone_recon():
     x = get_x_location()
     y = get_y_location()
 
-    # Empty list checker
-    if len(data) != 0: 
-        all_targets += data  # concatonates all data to all_targets (replacement for flattening)
-        for row in all_targets:
-            if (row[0] == 'base'):     #! Change this line to change what the bomber drone bombs
-                targets_to_hit.append(row)
-    else:   # Don't do anything with an empty list
-        pass
- 
     ## DRONE MOVES ##
 
     if taking_off():
-        set_destination(300, 500)
+        set_destination(500, 300)
 
     # If destination gets reached -> gets checked every frame
-    if destination_reached():
+    if destination_reached() and (x == 500 and y == 300):
         data = get_scan_results()
-        if x == 300 and y == 500:   # Scan 1
-            
-            set_destination(500, 500)
-        if x == 500 and y == 500:   # Scan 2
-            
-            set_destination(875, 500)
-        if x == 875 and y == 500:   # Scan 3
-            
-            set_destination(1250, 500)
-        if x == 1250 and y == 500:  # Scan 4
-            
-            set_destination(1625, 500)
-        if x == 1625 and y == 500:  # Scan 5
-            
-            set_destination(1625, 800)
-        if x == 1250 and y == 800:  # Scan 6
-            
-            set_destination(875, 800)
-        if x == 875 and y == 500:  # Scan 7
-            
-            set_destination(500, 800)
+        is_empty_scan(data)
+        set_destination(500, 500)
+
+    if destination_reached() and (x == 500 and y == 500):
+        data = get_scan_results()
+        is_empty_scan(data)
+        set_destination(875, 500)
+
+    if destination_reached() and (x == 875 and y == 500):
+        data = get_scan_results()
+        is_empty_scan(data)
+        set_destination(1250, 500)
+
+    if destination_reached() and (x == 1250 and y == 500):
+        data = get_scan_results()
+        is_empty_scan(data)
+        set_destination(1625, 500)
+
+    if destination_reached() and (x == 1625 and y == 500):
+        data = get_scan_results()
+        is_empty_scan(data)
+        set_destination(1625, 800)
+
+    if destination_reached() and (x == 1625 and y == 800):
+        data = get_scan_results()
+        is_empty_scan(data)
+        set_destination(875, 800)
+
+    if destination_reached() and (x == 875 and y == 800):
+        data = get_scan_results()
+        is_empty_scan(data)
+        set_destination(500, 800)
 
     if destination_reached() and (x == 500 and y == 800):   # When scan drone reaches (500, 800) commence bombing *insert 1812 overature here*
-        # = get_scan_results()
         bomb = True
 
     ## ============ ##
@@ -125,6 +138,7 @@ def drone_recon():
 kill_it = list()
 base_to_hit = 0     # Keeps track of what index to "pop"
 
+'''
 def drone_bomber():
     # "deploy_air_to_ground(x, y)" is instantaneous
     #! Bomber has 100 pixel radius
@@ -157,8 +171,8 @@ def drone_bomber():
 
             else:
                 mission_complete()
-
     
+'''
 
 # This loads the simulation scenario
 # DO NOT TOUCH
